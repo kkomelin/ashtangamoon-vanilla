f51427d5deae703f705c9b4334006b416558d47f
Konstantin Komelin <konstantin.komelin@gmail.com>
2020-07-21 21:55:19 +0300

    Improved webpack config.

--- webpack.common.js
+++ webpack.common.js
@@ -2,13 +2,22 @@ const path = require('path');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const distDir = path.resolve(__dirname, 'dist');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const TerserJSPlugin = require('terser-webpack-plugin');
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 
 module.exports = {
   entry: {
     app: './src/index.js',
   },
+  optimization: {
+    minimizer: [new TerserJSPlugin({}), new OptimizeCSSAssetsPlugin({})],
+  },
   plugins: [
-    // new CleanWebpackPlugin(['dist/*']) for < v2 versions of CleanWebpackPlugin
+    new MiniCssExtractPlugin({
+      filename: '[name].[contenthash].css',
+      chunkFilename: '[id].[contenthash].css',
+    }),
     new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
       title: 'Next Moon Phase',
@@ -22,8 +31,19 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.css$/i,
-        use: ['style-loader', 'css-loader'],
+        test: /\.css$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              // only enable hot in development
+              hmr: process.env.NODE_ENV === 'development',
+              // if hmr does not work, this is a forceful method.
+              // reloadAll: true,
+            },
+          },
+          'css-loader',
+        ]
       },
     ],
   }
