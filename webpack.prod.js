d0f752bcb28ba579c6326fd96c9e83180b233f39
Konstantin Komelin <konstantin.komelin@gmail.com>
2022-06-27 20:05:48 +0300

    Changed the app name to Ashtanga Holidays.

--- webpack.prod.js
+++ webpack.prod.js
@@ -7,38 +7,39 @@ const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
 
 module.exports = merge(common, {
-  mode: 'production',
+  mode: "production",
   optimization: {
     minimizer: [new TerserJSPlugin({}), new OptimizeCSSAssetsPlugin({})],
   },
   plugins: [
     new GenerateSW({
-      swDest: 'sw.js',
+      swDest: "sw.js",
       clientsClaim: true,
       skipWaiting: true,
     }),
     new WebpackPwaManifest({
-      name: 'Moon Peaks - When will the new/full moon occur?',
-      short_name: 'Moon Peaks',
-      description: 'Displays the next full and new moon date and visualizes current moon phase.',
-      background_color: '#193d61',
-      theme_color: '#193d61',
-      crossorigin: 'use-credentials', //can be null, use-credentials or anonymous
+      name: "Ashtanga Yoga Holidays - Upcoming full and new Moon dates",
+      short_name: "Ashtanga Yoga Holidays",
+      description:
+        "Displays next full and new moon dates and visualizes current moon phase.",
+      background_color: "#193d61",
+      theme_color: "#193d61",
+      crossorigin: "use-credentials", //can be null, use-credentials or anonymous
       icons: [
         {
-          src: path.resolve('src/img/large-icon.png'),
-          sizes: [96, 128, 192, 256, 384, 512] // multiple sizes
+          src: path.resolve("src/img/large-icon.png"),
+          sizes: [96, 128, 192, 256, 384, 512], // multiple sizes
         },
         {
-          src: path.resolve('src/img/large-icon.png'),
-          size: '1024x1024' // you can also use the specifications pattern
+          src: path.resolve("src/img/large-icon.png"),
+          size: "1024x1024", // you can also use the specifications pattern
         },
         // {
         //   src: path.resolve('src/assets/maskable-icon.png'),
         //   size: '1024x1024',
         //   purpose: 'maskable'
         // }
-      ]
-    })
+      ],
+    }),
   ],
 });
